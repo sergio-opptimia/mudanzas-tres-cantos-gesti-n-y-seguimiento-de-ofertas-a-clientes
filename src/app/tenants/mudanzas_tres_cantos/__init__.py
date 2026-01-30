9a26386d52d6d80d269bdd4c33296357a07d146a
sergio-opptimia <sergio@opptimia.com>
2026-01-30 12:44:25 +0100

    Add src/app/tenants/mudanzas_tres_cantos/__init__.py

---
+++ src/app/tenants/mudanzas_tres_cantos/__init__.py
@@ -0,0 +1,9 @@
+# src/app/tenants/mudanzas_tres_cantos/__init__.py
+"""Initialization file for the mudanzas_tres_cantos package."""
+
+# This file can be used to expose sub-modules or to perform package-level initialization.
+# For this project, it primarily serves to mark the directory as a Python package.
+# If an Agent class is defined within this package (e.g., in agent.py),
+# it can be imported here for easier access from other parts of the application.
+
+# Example: from .agent import MudanzasTresCantosAgent
